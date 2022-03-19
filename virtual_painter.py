afa9a82dd9a12056eb56163da9738dd0a6c7e165
NASEEM A P <naseemalassampattil@gmail.com>
2022-03-19 18:16:27 +0530

    created findColor function to find color, added color parameters into a list, checked mask display - working properly, for Blue color have a less clarity

--- virtual_painter.py
+++ virtual_painter.py
@@ -1,14 +1,26 @@
 import cv2
+import numpy as np
+
+color_parameters = [
+    # h_min s_min v_min h_max s_max v_max
+    [90, 86, 40, 133, 178, 79],    # Blue
+    [0, 126, 128, 32, 255, 255],   # Yellow
+    [33, 98, 81, 69, 255, 255]     # Green
+]
+
+
+def findColors(img, color_parameters):
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array(color_parameters[0][0:3])
+    upper = np.array(color_parameters[0][3:6])
+    mask = cv2.inRange(hsv_img, lower, upper)
+    cv2.imshow("Mask", mask)
 
 cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    img_copy = img.copy()
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur_img = cv2.GaussianBlur(img, (5, 5), 1)
-    canny_img = cv2.Canny(blur_img, 50, 50)
-    cv2.imshow("Image Copy", img_copy)
-    cv2.imshow("Gray", gray_img)
-    cv2.imshow("Canny", canny_img)
+
+    findColors(img, color_parameters)
+    cv2.imshow('Result', img)
     cv2.waitKey(1)
